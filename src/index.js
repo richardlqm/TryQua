83b34fb64cf9001b6d3f2a9d70f64006b022a369
Victor R. Escobar <victor.escobar@quantum-machines.co>
2023-09-12 14:51:41 +0200

    merged changes and webpack configured

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log("hello from js");
 
 // fetch a file "hello.py" and read the string, set it on tag
-fetch("hello.py").then((response) => {
+fetch("static/hello.py").then((response) => {
   response.text().then((text) => {
     console.log(text);
     // document.querySelector("#hello").innerText = text;
