0ba47accb84f0a58e4b4f54ffdb0901ea5eaf190
Victor R. Escobar <victor.escobar@quantum-machines.co>
2023-09-13 13:13:39 +0200

    dynamically generated files

--- src/index.js
+++ src/index.js
@@ -60,8 +60,16 @@ class PythonEvaluator {
     this.resultEl.innerText = this.pyodide.runPython(this.codeEl.textContent);
   }
 
-  quaLoader(pckg) {
-    return this.pyodide.loadPackage(`static/python-examples/packages/${pckg}`);
+  quaLoader(packages) {
+    var arr = packages.map((pckg) =>
+      this.pyodide.loadPackage(`static/python-examples/packages/${pckg}`)
+    );
+    var dstpckgs = ["numpy", "setuptools", "ssl"];
+    dstpckgs.forEach((dist_pckg) => {
+      console.log("adding: " + dist_pckg);
+      arr.push(this.pyodide.loadPackage(dist_pckg));
+    });
+    return Promise.all(arr);
   }
 }
 
@@ -86,45 +94,11 @@ function setupLinks({ files, filesEl, codeEl, descrEl, titleEl }) {
 async function main() {
   // @ts-ignore
   let pyodide = loadPyodide();
+  let files = await (
+    await fetch("static/python-examples/examples.json")
+  ).json();
   let setupElements = {
-    files: [
-      {
-        title: "Hello World",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.lo world program for the broser",
-        name: "hello.py",
-      },
-      {
-        title: "Calculation",
-        description:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo",
-        name: "calculation.py",
-      },
-      {
-        title: "CPMG",
-        description:
-          " Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt",
-        name: "cpmg.py",
-      },
-      {
-        title: "Frame Rotation",
-        description:
-          "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-        name: "frame_rotation.py",
-      },
-      {
-        title: "Phase Coherence",
-        description:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        name: "phase_coherence.py",
-      },
-      {
-        title: "Power Rabi example",
-        description:
-          "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-        name: "power_rabi.py",
-      },
-    ],
+    files: files,
     codeEl: document.getElementById("quaCode"),
     resultEl: document.getElementById("result"),
     titleEl: document.getElementById("title"),
@@ -141,12 +115,13 @@ async function main() {
   let fileHandler = new FileHandler(setupElements);
 
   fileHandler.loadFile(setupElements.files[0]);
-  pyEval.quaLoader("qua_emulator.whl").then(() => {
-    pyEval.quaLoader("fxpmath.whl").then(() => {
-      // @ts-ignore
-      runButton.disabled = false;
-      runButton?.addEventListener("click", () => pyEval.evaluatePython());
-    });
+  let pckgs = await (
+    await fetch("/static/python-examples/packages/pckgindex.json")
+  ).json();
+  pyEval.quaLoader(pckgs).then(() => {
+    // @ts-ignore
+    runButton.disabled = false;
+    runButton?.addEventListener("click", () => pyEval.evaluatePython());
   });
 }
 
